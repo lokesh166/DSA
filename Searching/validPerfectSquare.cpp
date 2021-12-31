ec5b0b2d07ae1b61ce9786f3a8a16e12f925f7b7
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2021-12-31 20:07:25 +0530

    Create validPerfectSquare.cpp

---
+++ Searching/validPerfectSquare.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num == 1)
+        return true;
+
+    int start = 0;
+    int end = num - 1;
+    long long mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+        if (mid * mid == num)
+            return true;
+
+        else if (mid * mid > num)
+        {
+            end = mid - 1;
+        }
+        else if (mid * mid < num)
+        {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+    return false;
+    }
+};
