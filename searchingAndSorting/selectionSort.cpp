fd8546d8d4d421e9c415dd3b0d669610932713e5
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-01-22 14:27:46 +0530

    Update selectionSort.cpp

--- searchingAndSorting/selectionSort.cpp
+++ searchingAndSorting/selectionSort.cpp
@@ -1,13 +1,32 @@
-void selectionSort(vector<int>& arr, int n)
-{   
-    // Write your code here.
+#include<iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n){
     for(int i=0; i<n-1; i++){
-            int minIndex = i;
-            for(int j=i+1; j<n; j++){
-				if(arr[minIndex] > arr[j]){
-                    minIndex = j;
-                }
+// 	    below function only how to sorting is done by printing arr
+//             for(int i=0; i<6; i++){
+//             cout << arr[i] << " ";
+//             }cout << endl;
+// 	
+        int min = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[min] > arr[j]){
+                min = j;
             }
-            swap(arr[minIndex], arr[i]);
         }
+        swap(arr[min], arr[i]);
+    }
+}
+
+int main(){
+
+
+    int arr[] = {4,9,10,2,1,5};
+
+    selectionSort(arr, 6);
+
+    for(int i=0; i<6; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
 }
