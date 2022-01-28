ea9fa7755d720fa7d6ea1aa5c760974a2ba5b310
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-01-28 16:15:35 +0530

    Update pattern4.cpp

--- Pattern/pattern4.cpp
+++ Pattern/pattern4.cpp
@@ -24,3 +24,33 @@ int main(){
     }
     return 0;
 }
+
+
+
+
+output
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1 
+5 4 3 2 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i <= n){
+        int j = n;
+        while(j >= 1){
+            cout << j << " ";
+            j--;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
