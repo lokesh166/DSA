82589a6d43c2a1edb8ed3037da33ceb57af65612
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-01-28 15:53:31 +0530

    Update pattern1.cpp

--- Pattern/pattern1.cpp
+++ Pattern/pattern1.cpp
@@ -12,3 +12,36 @@ int main(){
     }
 }
 
+
+
+// output
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * *
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int i = 0;
+    
+
+    while(i < n){
+        int j = 0;
+        while(j < n){
+            cout << "*" << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}
