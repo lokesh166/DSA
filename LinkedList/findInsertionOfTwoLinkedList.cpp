73b11cd4df6e641a974f5e880f3ec3e725aefbae
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-01-30 14:20:12 +0530

    Update findInsertionOfTwoLinkedList.cpp

--- LinkedList/findInsertionOfTwoLinkedList.cpp
+++ LinkedList/findInsertionOfTwoLinkedList.cpp
@@ -26,3 +26,68 @@ public:
         
     }
 };
+
+
+
+
+// apporace 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+
+int length(ListNode* head){
+    ListNode* temp = head;
+    int count = 0;
+    while(temp != NULL){
+        count++;
+        temp = temp -> next;
+    }
+    return count;
+}
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        if(headA == NULL || headB == NULL){
+            return NULL;
+        }
+        
+        int l1 = length(headA);
+        int l2 = length(headB);
+        
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+        
+        
+        if(l1 > l2){
+            
+            int d = l1-l2;
+            
+            while(d){
+                temp1 = temp1 -> next;
+                d--;
+            }
+            
+        }else{
+            int d = l2-l1;
+            while(d){
+                temp2 = temp2 -> next;
+                d--;
+            }
+        }
+        
+        while(temp1 != temp2){
+           temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
+        }
+        
+        return temp1;
+    }
+};
