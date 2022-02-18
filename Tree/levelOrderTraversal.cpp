7d9361c8d44f432199850b8d599e5d49cd71fa37
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-02-18 21:18:56 +0530

    Update levelOrderTraversal.cpp

--- Tree/levelOrderTraversal.cpp
+++ Tree/levelOrderTraversal.cpp
@@ -1,24 +1,46 @@
-vector<vector<int>> levelOrder(TreeNode* root) {
-        if(root==NULL)
-        return {};
-    queue<TreeNode*> q;
-    vector<vector<int>> ans;
-    q.push(root);
-    while(q.size()){
-        vector<int> temp;
-        int count=q.size();
-        while(count){
-            TreeNode* front=q.front();
-            q.pop();
-            temp.push_back(front->val);
-            if(front->left)
-                q.push(front->left);
-            if(front->right)
-                q.push(front->right);
-            count--;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        if(root == NULL){
+            return result;
         }
-        ans.push_back(temp);
-    }
-    return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            vector<int> ans;
+            int count = q.size();
+            
+            while(count){
+                
+                TreeNode* frontNode = q.front();
+                q.pop();
+                
+                ans.push_back(frontNode->val);
+                
+                if(frontNode->left)
+                    q.push(frontNode->left);
+                
+                if(frontNode->right)
+                    q.push(frontNode->right);
+                
+                count--;
+            }
+            result.push_back(ans);
+        }
+        return result;
     }
 };
