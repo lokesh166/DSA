1f1b98450e598b202d377540d89b7feb436ed449
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-19 21:09:03 +0530

    Update checkCircularlyLinkedList.cpp

--- LinkedList/checkCircularlyLinkedList.cpp
+++ LinkedList/checkCircularlyLinkedList.cpp
@@ -14,6 +14,9 @@
         }
 **************************************************/
 
+
+// it will give time limit exceed at some platform 
+
 bool isCircular(Node* head){
     if(head == NULL){
         return true;
@@ -29,4 +32,46 @@ bool isCircular(Node* head){
     }else{
         return false;
     }
+}
+
+
+
+// it does not give TLE
+
+/*************************************************
+        Following is the structure of class Node:
+     
+        class Node{
+        public:
+            int data;
+            Node* next;
+            
+            Node(int data){
+                this->data = data;
+                this->next = NULL;
+            }
+            
+        }
+**************************************************/
+
+bool isCircular(Node* head){
+    if(head == NULL) return true;
+    
+    if(head->next == NULL) return false;
+    
+    if(head-> next  == head) return true;
+    
+    Node* temp = head->next;
+    head->data = 0;
+    
+    while(temp != NULL && temp->data != 0){
+		temp->data = 0;
+        temp = temp->next;
+    }
+    
+    if(temp == NULL) return false;
+    
+    if(temp->data == 0 && temp != head) return false;
+    
+    return true;
 }
