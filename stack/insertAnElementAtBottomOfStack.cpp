3daf6662d369ea463e504ead2a74416b5492953a
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-21 16:31:54 +0530

    Create insertAnElementAtBottomOfStack.cpp

---
+++ stack/insertAnElementAtBottomOfStack.cpp
@@ -0,0 +1,25 @@
+
+void solve(stack<int>& myStack, int x){
+    
+//     base case
+    if(myStack.empty()){
+        myStack.push(x);
+        return;
+    }
+    
+    int num = myStack.top();
+    myStack.pop();
+    
+//     recursion call
+    
+    solve(myStack, x);
+    
+    myStack.push(num);
+}
+
+
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    solve(myStack, x);
+    return myStack;
+}
