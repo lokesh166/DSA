7a463aa6823698be345b3df4257535a03c5be8de
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-22 18:19:43 +0530

    Create heapSort.cpp

---
+++ Heap/heapSort.cpp
@@ -0,0 +1,42 @@
+void heapify(int arr[] , int n,  int i){
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i + 1;
+
+        if(left <= n && arr[largest] < arr[left]){
+            largest = left;
+        }
+        if(right <= n && arr[largest] < arr[right]){
+            largest = right;
+        }
+
+        if(largest != i ){
+            swap(arr[largest], arr[i]);
+            heapify(arr, n, largest);
+        }
+}
+
+
+void heapSort(int arr[], int n){
+    int size = n;
+    while(size > 1){
+        // step1: swap 
+        swap(arr[size], arr[1]);
+
+        // step2: size decrease
+        size--;
+
+        // step3: hepify algo
+        heapify(arr, size, 1);
+    }
+}
+
+
+
+heapSort(arr, n);
+
+
+    for(int i=1; i<=n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
