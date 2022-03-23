0695cabf6128a86a8987603943d4ea665b518330
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-23 10:12:54 +0530

    Update pattern15.cpp

--- Pattern/pattern15.cpp
+++ Pattern/pattern15.cpp
@@ -42,3 +42,55 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+// output
+
+/*
+
+5
+A B C D E
+B C D E F
+C D E F G
+D E F G H
+E F G H I
+
+*/
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    
+    int i=1;
+    char ch = 'A';
+    while(i <= n){
+        int j=1;
+        while(j <= n){
+             char ch = 'A' + i + j -2;
+            // 'A' ascii value is 65
+            cout << ch <<  " ";
+            ch++;
+            
+            j++;
+            
+            
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
