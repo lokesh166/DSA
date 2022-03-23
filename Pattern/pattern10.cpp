88f93117a88dcecfd6da979ac0a6740de1b41d2a
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-23 09:41:05 +0530

    Update pattern10.cpp

--- Pattern/pattern10.cpp
+++ Pattern/pattern10.cpp
@@ -37,3 +37,44 @@ int main(){
 
     return 0;
 }
+
+
+// another method without using extra count space 
+#include<iostream>
+using namespace std;
+
+
+// output
+
+/*
+
+5
+1
+2 3
+3 4 5
+4 5 6 7
+5 6 7 8 9
+
+*/
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    
+    int i=1;
+    while(i<=n){
+        int j=i;
+        while(j < i*2){
+            cout << j << ' ';
+            j++;
+            
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
