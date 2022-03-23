2564755f7c4ec6d547f8f286e8dfd5be898650ea
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-23 20:02:53 +0530

    Create pattern31.cpp

---
+++ Pattern/pattern31.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+
+// output
+
+/*
+5
+    * * * * *
+   * * * * *
+  * * * * *
+ * * * * *
+* * * * *
+
+*/
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    
+    int i = 1;
+    while(i <= n){
+
+        int j = 1;
+        while(j <= n-i){
+            cout << " ";
+            j++;
+        }
+
+        j = 1;
+        while(j <= n){
+            cout << "* ";
+            j++;
+        }
+
+        i++;
+        cout << endl;
+    }
+
+
+    return 0;
+}
