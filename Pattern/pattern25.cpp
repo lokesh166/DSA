bb7e3aec6a401a37f57b420d80faf1d56dc80fbe
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-23 10:45:52 +0530

    Create pattern25.cpp

---
+++ Pattern/pattern25.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+// output
+
+/*
+4
+   1
+  23
+ 456
+78910
+
+*/
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    
+    int i=1;
+    int count = 1;
+    while(i <= n){
+
+        int space = 1;
+        while(space <= n-i){
+            cout << " ";
+            space++;
+        }
+
+        int j=1;
+        while(j <= i){
+            cout << count;
+            j++;
+            count++;
+        }
+        i++;
+        cout << endl;
+    }
+
+    return 0;
+}
