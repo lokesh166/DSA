69c4d67ec2ad7ddf9af081596e4e4e323fc4f7ca
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-23 10:11:19 +0530

    Create pattern15.cpp

---
+++ Pattern/pattern15.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+
+// output
+
+/*
+
+5
+A B C D E
+B C D E F
+C D E F G
+D E F G H
+E F G H I
+
+*/
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    
+    int i=1;
+    char ch = 'A';
+    while(i <= n){
+        char ch = 'A' + i -1;
+        int j=1;
+        while(j <= n){
+            // 'A' ascii value is 65
+            cout << ch <<  " ";
+            ch++;
+            
+            j++;
+            
+            
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
