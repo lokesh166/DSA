ef93b39deee0d54f030e19e1244449ab6d128cb6
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-27 19:09:16 +0530

    Create pivotIndex.cpp

---
+++ Array/pivotIndex.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int right = 0;
+        int left = 0;
+        for(int i=0; i<n; i++){
+            right += nums[i];
+        }
+        for(int i=0; i<n; i++){
+            right -= nums[i];
+            if(right == left){
+                return i;   
+            }
+            left += nums[i];
+        }
+        return -1;
+    }
+};
