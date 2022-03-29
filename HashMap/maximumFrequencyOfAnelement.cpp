af6685c55caaab8997c8b43404f5eb2ca57aa143
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-03-29 13:50:14 +0530

    Create maximumFrequencyOfAnelement.cpp

---
+++ HashMap/maximumFrequencyOfAnelement.cpp
@@ -0,0 +1,20 @@
+int maximumFrequency(vector<int> &arr, int n)
+{
+    unordered_map<int, int> mp;
+    
+    int maxFreq = 0;
+    int maxAns = 0;
+    
+    for(int i=0; i<arr.size(); i++){
+        mp[arr[i]]++;
+        maxFreq = max(maxFreq, mp[arr[i]]);
+    }
+    
+    for(int i=0; i<arr.size(); i++){
+		if(maxFreq == mp[arr[i]]){
+			maxAns = arr[i];
+            break;
+        }
+    }
+    return maxAns;
+}
