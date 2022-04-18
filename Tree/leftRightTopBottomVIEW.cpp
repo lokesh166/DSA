dd88a6c4056834e61824eef5fb7ef6e45c45ad99
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-04-18 18:59:23 +0530

    Update leftRightTopBottomVIEW.cpp

--- Tree/leftRightTopBottomVIEW.cpp
+++ Tree/leftRightTopBottomVIEW.cpp
@@ -50,3 +50,47 @@ public:
         
     }
 };
+
+
+
+
+
+
+// top view of the binary tree
+
+
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+       vector<int> ans;
+       queue<pair<int, Node*>> q;
+       map<int, int> mp;
+       
+       q.push({0, root});
+       
+       while(!q.empty()){
+           auto p = q.front();
+           q.pop();
+           
+           int index = p.first;
+           Node* node = p.second;
+           
+           if(mp.find(index) == mp.end()){
+               mp[index] = node->data;
+           }
+           
+           if(node->left) q.push({index-1, node->left});
+           
+           if(node->right) q.push({index+1, node->right});
+       }
+       for(auto it: mp){
+           ans.push_back(it.second);
+       }
+       return ans;
+    }
+
+};
