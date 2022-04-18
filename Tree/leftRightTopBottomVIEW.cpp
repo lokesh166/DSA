5c261f7b248b302cf268ab6712d6143f7797d7f0
lokesh166 <78734482+lokesh166@users.noreply.github.com>
2022-04-18 19:18:55 +0530

    Update leftRightTopBottomVIEW.cpp

--- Tree/leftRightTopBottomVIEW.cpp
+++ Tree/leftRightTopBottomVIEW.cpp
@@ -94,3 +94,38 @@ class Solution
     }
 
 };
+
+
+
+// bottom view of the binary tree
+
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+       
+       vector<int> ans;
+       if(root == NULL) return ans;
+       
+       map<int, int> mp;
+       queue<pair<Node*, int>> q;
+       
+       q.push({root, 0});
+       
+       while(!q.empty()){
+           auto p = q.front();
+           q.pop();
+           
+           Node* node = p.first;
+           int index = p.second;
+           
+           mp[index] = node->data;
+           
+           if(node->left) q.push({node->left, index-1});
+           if(node->right) q.push({node->right, index+1});
+       }
+       for(auto it: mp){
+           ans.push_back(it.second);
+       }
+       return ans;
+    }
+};
